df18a6e1fa7eb9ebee374fb68747f5a90c8a8b50
mdm717 <33270132+mdm717@users.noreply.github.com>
2018-11-15 17:17:34 -0500

    Added some user functionality

--- src/Controllers/UserController.java
+++ src/Controllers/UserController.java
@@ -1,10 +1,3 @@
-/* 
- * AdminController Class
- * 
- * 		This class creates the main UI for the user to add, edit, and
- * 		delete Users from a list
- * 
- */
 package Controllers;
 	
 import java.io.BufferedReader;
@@ -14,6 +7,8 @@ import java.io.IOException;
 import java.util.Optional;
 
 import com.sun.glass.events.WindowEvent;
+
+import resources.Album;
 
 import javafx.application.Application;
 import javafx.application.Platform;
@@ -28,6 +23,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextInputDialog;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextInputDialog;
@@ -39,8 +36,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 
 public class UserController extends Application {
-	public static final ObservableList<User> data = FXCollections.observableArrayList();
-	private static ListView<User> listView;
+	public static final ObservableList<Album> data = FXCollections.observableArrayList();
+	private static ListView<Album> listView;
+	
 	private static Stage mainStage;
 	private static Scene scene;
 
@@ -58,6 +56,9 @@ public class UserController extends Application {
 			Parent root= FXMLLoader.load(getClass().getResource("PhotoLibSub.fxml"));
 			scene = new Scene(root);
 			
+			listView = (ListView<Album>) scene.lookup("#list");
+			listView.setItems(data);
+	
 			primaryStage.setOnCloseRequest(e -> {
 		        Platform.exit();
 		        System.exit(0);
@@ -68,10 +69,56 @@ public class UserController extends Application {
         } catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
-		}
+		} 
 			
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		mainStage= primaryStage;
 	}
+
+	@FXML
+	public void create(ActionEvent e) {
+		TextInputDialog dialog = new TextInputDialog();
+		dialog.setTitle("Album Name Input");
+		dialog.setHeaderText("Enter Album Name: ");
+		dialog.showAndWait();
+		if(dialog.getEditor().getText().equals(null)) {
+			System.out.println("OH");
+		}
+		else {
+			data.add(new Album(dialog.getEditor().getText().toString()));
+			index++;
+			//sort();
+		}
+		
+	}
+	
+	public void delete(ActionEvent e) {
+		int i = listView.getSelectionModel().getSelectedIndex();
+		Alert alert = new Alert(AlertType.CONFIRMATION);
+		alert.setTitle("Confirm Delete");
+		alert.setHeaderText("Really Delete This Album?");
+		Optional<ButtonType> result = alert.showAndWait();
+		if(result.get() == ButtonType.OK) {
+			data.remove(listView.getSelectionModel().getSelectedIndex());
+			try {
+				listView.getSelectionModel().select(i-1);
+			} catch(Exception e2) {}
+		}
+	}
+	
+	public void open(ActionEvent e) {
+		
+	}
+	
+	public void rename(ActionEvent e) {
+		TextInputDialog dialog = new TextInputDialog();
+		dialog.setTitle("Rename Album");
+		dialog.setHeaderText("New Album Name: ");
+		Optional<String> result = dialog.showAndWait();
+		if(result.isPresent()) {
+			
+		}
+	}
+	
 }
